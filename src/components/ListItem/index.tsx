b064af7f29a517acc6334243c2cee11b8253879d
Elton <eltonjf@gmail.com>
2021-10-11 02:26:28 -0300

    Add and check working

---
+++ src/components/ListItem/index.tsx
@@ -0,0 +1,33 @@
+import { useState }from 'react';
+import * as  C from './styles';
+import { Item } from '../../types/Item';
+
+type Props = {
+    item: Item;
+}
+
+export const ListItem = ({item}: Props) => {
+
+    const [isChecked, setIsChecked] = useState(item.done);
+
+    const handleDone = (done: boolean) => {
+        if (done === true ){
+            item.done = true;
+            setIsChecked(true);
+        }else{
+            item.done = false;
+            setIsChecked(false);
+        }
+    }
+
+    return(
+        <C.Container done={isChecked}>
+            <input 
+                type="checkbox" 
+                checked={isChecked}
+                onChange={e => handleDone(e.target.checked)}
+            />
+            <label>{item.name} - {item.done.toString()}</label>
+        </C.Container>
+    );
+}
